5e2b24ae93c5d3442b1a0f2ab89e4adc130e2234
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2023-08-10 22:59:49 +0700

    update-adm1n

--- src/ecommerce-admin/pages/index.js
+++ src/ecommerce-admin/pages/index.js
@@ -1,17 +1,18 @@
 import Layout from "@/components/Layout";
-import { useSession } from "next-auth/react";
+import { useSession, signIn, signOut  } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession();
-  return (
-    <Layout>
-      <div className="text-blue-900 flex justify-between">
-        <h2>Hello, {session?.user?.name}</h2>
-        <div className="flex bg-gray-300 gap-1 text-black">
-          <img src={session?.user?.image} alt="" className="w-8 h-8" />
-          <span className="py-1 px-2">{session?.user?.name}</span>
+    const {data: session} = useSession();
+    console.log({session});
+    return <Layout >
+        test
+        <div className="text-white">
+            <title>US Coffee</title>
+            <h2>
+                Hello, {session?.user?.name}
+            </h2>
+            hi
         </div>
-      </div>
     </Layout>
-  );
 }
+
